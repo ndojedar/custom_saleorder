dfb366810b3f71a13be40073af77234a09c3f7f1
Nestor Ojeda <nojeda@adasoft.es>
2022-04-11 12:53:30 +0200

    minimo

--- models/sale_order_model.py
+++ models/sale_order_model.py
@@ -7,9 +7,10 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     sales_va = fields.Monetary(
-          comodel_name='project.project',  
-          related='analytic_account_id.project_ids.sale_added_value_economic_ratio', 
-          string='Sales VA',
-          readonly=True, 
-          store=True
-          )
+        comodel_name='project.project',  
+        related='analytic_account_id.project_ids.sale_added_value_economic_ratio', 
+        string='Sales VA',
+        readonly=True, 
+        store=True
+    )
+
